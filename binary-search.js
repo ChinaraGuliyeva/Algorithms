9f465a2ef0d86ec9a9f67c1c39e23274aaf7f6ae
Chinara Guliyeva <ichinara@gmail.com>
2022-09-05 21:40:23 +0400

    Added realization for binary search

---
+++ binary-search.js
@@ -0,0 +1,26 @@
+// O(log n)
+// For sorted lists
+
+const binarySearch = (list, item) => {
+    let low = 0;
+    let high = list.length - 1;
+    let mid;
+    let guess;
+
+    while(low <= high){
+        mid = Math.floor((low + high) / 2);
+        guess = list[mid];
+        if(guess == item){
+            return mid;
+        } else if (guess > item){
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return null;
+}
+
+const list = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(list, 9));
